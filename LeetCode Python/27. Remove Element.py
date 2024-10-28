9274826277858747df7a53c254389457b001d135
harold1735 <harold.eustaquio.a@uni.pe>
2024-10-27 20:51:34 -0600

    array challenges: starting...

--- LeetCode Python/27. Remove Element.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        x = nums.copy()
-        for num in x:
-            if(val in nums):
-                nums.remove(val)
-        return len(nums)
--- LeetCode Python/9. Palindrome Number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
-
-        temp = str(x)
-
-        return int(temp[::-1]) == x
